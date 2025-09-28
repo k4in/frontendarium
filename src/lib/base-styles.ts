682e71b3f24f395a28bf71bb18704cbabb755733
k4in <f.rehner@mac.com>
2025-09-29 00:51:52 +0200

    baseUi + select

--- src/lib/base-styles.ts
+++ src/lib/base-styles.ts
@@ -1,7 +1,14 @@
+import { cn } from '@/lib/utils/cn';
+
 export const FOCUS_STYLES =
-  'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-transparent';
+  'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-background';
 
 export const DISABLED_STYLES =
   'disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-70 disabled:text-muted-foreground disabled:shadow-none';
 
-export const INTERACTIVE_STYLES = `${FOCUS_STYLES} ${DISABLED_STYLES}`;
+const DATA_DISABLED_STYLES =
+  'data-[disabled]:pointer-events-none data-[disabled]:cursor-not-allowed data-[disabled]:opacity-70 data-[disabled]:text-muted-foreground data-[disabled]:shadow-none';
+
+export const INTERACTIVE_STYLES = cn(FOCUS_STYLES, DISABLED_STYLES);
+
+export const INTERACTIVE_DATA_STYLES = cn(FOCUS_STYLES, DATA_DISABLED_STYLES);
--- src/routes/components/select.tsx
+++ src/routes/components/select.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { SelectComponent } from '@/components/ui/select';
+import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/components/select')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/components/select"!</div>
+  return (
+    <div className="flex items-center gap-2">
+      <SelectComponent />
+      <Button>Hello</Button>
+    </div>
+  );
 }
